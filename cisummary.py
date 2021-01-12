30b21c4d1a67be94398a30f9aae98e910444efe9
Danny Zhu <dzhu@determined.ai>
2021-01-11 20:55:20 -0800

    Parallelize network requests for status summary

--- cisummary.py
+++ cisummary.py
@@ -3,8 +3,10 @@
 import enum
 import json
 import os
+import queue
 import re
 import sys
+import threading
 import time
 from collections import defaultdict
 
@@ -26,19 +28,6 @@ for name, attrs in [
     )
 
 
-old_safe_attr_name = parser.PyxlParser.safe_attr_name
-
-
-# @staticmethod
-# def safe_attr_name(name):
-#     if name == "from":
-#         return "xfrom"
-#     return old_safe_attr_name(name)
-
-
-# parser.PyxlParser.safe_attr_name = safe_attr_name
-
-
 def cached(enable, fn, func, *args, **kwargs):
     if enable and os.path.exists(fn):
         with open(fn) as f:
@@ -56,17 +45,11 @@ def parse_time(s):
     return time.strptime(s, "%Y-%m-%dT%H:%M:%S")
 
 
-# categorize branches (master, pull/*, other)
-# group up seen workflows and seen job for each workflow
-# order pipelines by time and output row for each
-
-
 class SVG:
     success = <svg style="color: rgb(4, 155, 74);" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" class="icon"><path d="M12,2 C6.48,2 2,6.48 2,12 C2,17.52 6.48,22 12,22 C17.52,22 22,17.52 22,12 C22,6.48 17.52,2 12,2 L12,2 Z M10,17 L5,12 L6.41,10.59 L10,14.17 L17.59,6.58 L19,8 L10,17 L10,17 Z"></path></svg>
     failed = <svg style="color: rgb(242, 70, 70);" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" class="icon"><path d="M12,2 C6.48,2 2,6.48 2,12 C2,17.52 6.48,22 12,22 C17.52,22 22,17.52 22,12 C22,6.48 17.52,2 12,2 L12,2 Z M13,17 L11,17 L11,15 L13,15 L13,17 L13,17 Z M13,13 L11,13 L11,7 L13,7 L13,13 L13,13 Z"></path></svg>
     blocked = <svg style="color: rgb(127, 127, 127);" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" class="icon"><path fill-rule="evenodd" d="M2,12 C2,17.52 6.48,22 12,22 C17.52,22 22,17.52 22,12 C22,6.48 17.52,2 12,2 C6.48,2 2,6.48 2,12 Z M7,10.5 C6.17,10.5 5.5,11.17 5.5,12 C5.5,12.83 6.17,13.5 7,13.5 C7.83,13.5 8.5,12.83 8.5,12 C8.5,11.17 7.83,10.5 7,10.5 Z M17,10.5 C16.17,10.5 15.5,11.17 15.5,12 C15.5,12.83 16.17,13.5 17,13.5 C17.83,13.5 18.5,12.83 18.5,12 C18.5,11.17 17.83,10.5 17,10.5 Z M12,10.5 C11.17,10.5 10.5,11.17 10.5,12 C10.5,12.83 11.17,13.5 12,13.5 C12.83,13.5 13.5,12.83 13.5,12 C13.5,11.17 12.83,10.5 12,10.5 Z"></path></svg>
     running = <svg style="color: rgb(53, 149, 220);" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" class="icon"><g name="icon-workflows-running" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><circle name="Oval" fill="#3AA3F2" cx="12" cy="12" r="10"></circle><path d="M22,12 C22,6.4771525 17.5228475,2 12,2 C6.4771525,2 2,6.4771525 2,12" name="Shape" fill="#D7ECFC"></path><path d="M22,12 C22,6.4771525 17.5228475,2 12,2 C6.4771525,2 2,6.4771525 2,12" name="Shape" fill="#3AA3F2"></path></g></svg>
-    # running = <svg style="color: rgb(53, 149, 220);" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" class="css-icon"><g name="icon-workflows-running" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><circle name="Oval" fill="#3AA3F2" cx="12" cy="12" r="10"></circle><path d="M22,12 C22,6.4771525 17.5228475,2 12,2 C6.4771525,2 2,6.4771525 2,12" name="Shape" fill="#D7ECFC"><animateTransform attributeName="transform" type="rotate" from="0 12 12" to="360 12 12" dur="2s" repeatCount="indefinite"></animateTransform></path><path d="M22,12 C22,6.4771525 17.5228475,2 12,2 C6.4771525,2 2,6.4771525 2,12" name="Shape" fill="#3AA3F2"><animateTransform attributeName="transform" type="rotate" from="180 12 12" to="540 12 12" dur="4s" repeatCount="indefinite"></animateTransform></path></g></svg>
     on_hold = <svg style="color: rgb(166, 146, 236);" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" class="icon"><path fill-rule="evenodd" d="M12,2 C6.48,2 2,6.48 2,12 C2,17.52 6.48,22 12,22 C17.52,22 22,17.52 22,12 C22,6.48 17.52,2 12,2 Z M11,16 L9,16 L9,8 L11,8 L11,16 Z M15,16 L13,16 L13,8 L15,8 L15,16 Z"></path></svg>
     canceled = <svg style="color: rgb(127, 127, 127);" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" class="icon"><path d="M12,2 C6.48,2 2,6.48 2,12 C2,17.52 6.48,22 12,22 C17.52,22 22,17.52 22,12 C22,6.48 17.52,2 12,2 L12,2 Z M17,13 L7,13 L7,11 L17,11 L17,13 L17,13 Z"></path></svg>
 
@@ -197,6 +180,41 @@ class Selection(enum.Enum):
     Pulls = "pulls"
 
 
+def worker(in_q, out_q):
+    while True:
+        (task, args) = in_q.get()
+        if task is None:
+            in_q.task_done()
+            break
+
+        if task == "pipelines":
+            sel, page, token = args
+            if sel == Selection.Master:
+                pipelines = circleci.project_pipelines("master", page_token=token)
+            else:
+                pipelines = circleci.pipelines(page_token=token)
+
+            for pipeline in pipelines["items"]:
+                out_q.put(("pipeline", (pipeline,)))
+                in_q.put(("pipeline_workflows", (pipeline,)))
+            if page < 5:
+                in_q.put(("pipelines", (sel, page + 1, pipelines["next_page_token"])))
+
+        elif task == "pipeline_workflows":
+            (pipeline,) = args
+            workflows = circleci.pipeline_workflows(pipeline["id"])
+            out_q.put(("pipeline_workflows", (pipeline, workflows["items"])))
+            for workflow in workflows["items"]:
+                in_q.put(("workflow_jobs", (workflow,)))
+
+        elif task == "workflow_jobs":
+            (workflow,) = args
+            jobs = circleci.workflow_jobs(workflow["id"])
+            out_q.put(("workflow_jobs", (workflow, jobs["items"])))
+
+        in_q.task_done()
+
+
 def main(args):
     if 0:
         s = json.load(open("structure.json"))
@@ -210,61 +228,35 @@ def main(args):
 
     print("selection:", sel, file=sys.stderr)
 
-    page_token = None
-
     pipelines_map = {}
     workflows_map = {}
     jobs_map = {}
 
-    for page in range(6):
-        print("starting page", page + 1, file=sys.stderr)
-        if sel == Selection.Master:
-            pipelines = circleci.project_pipelines("master", page_token=page_token)
-        else:
-            pipelines = circleci.pipelines(page_token=page_token)
+    in_q = queue.Queue()
+    out_q = queue.Queue()
 
-        for pipeline in pipelines["items"]:
-            if sel == Selection.Pulls and not pipeline["vcs"].get(
-                "branch", ""
-            ).startswith("pull/"):
-                continue
+    in_q.put(("pipelines", (sel, 0, None)))
+    for _ in range(16):
+        t = threading.Thread(target=worker, args=(in_q, out_q))
+        t.daemon = True
+        t.start()
 
-            pipelines_map[pipeline["number"]] = pipeline
-            info = circleci.pipeline(pipeline["number"])
-            print(
-                "pipeline",
-                pipeline["number"],
-                info["vcs"].get("branch", info["vcs"].get("tag")),
-                file=sys.stderr,
-            )
+    in_q.join()
 
-            workflows = circleci.pipeline_workflows(pipeline["id"])
-            workflows_map[pipeline["id"]] = workflows["items"]
-            for workflow in workflows["items"]:
-                print(
-                    "  workflow",
-                    workflow["name"],
-                    workflow["status"],
-                    workflow["id"],
-                    file=sys.stderr,
-                )
-                jobs = circleci.workflow_jobs(workflow["id"])
-                jobs_map[workflow["id"]] = jobs["items"]
-                for job in jobs["items"]:
-                    c = {
-                        "success": 32,
-                        "failed": 31,
-                        "running": 34,
-                        "canceled": 90,
-                        "on_hold": 35,
-                        "blocked": 90,
-                        "queued": 90,
-                    }[job["status"]]
-                    print(f"    \x1b[{c}m{job['name']}\x1b[m", file=sys.stderr)
-
-        page_token = pipelines["next_page_token"]
-        if page_token is None:
+    while True:
+        try:
+            task, ret = out_q.get_nowait()
+        except queue.Empty:
             break
+        if task == "pipeline":
+            (pipeline,) = ret
+            pipelines_map[pipeline["number"]] = pipeline
+        elif task == "pipeline_workflows":
+            pipeline, workflows = ret
+            workflows_map[pipeline["id"]] = workflows
+        elif task == "workflow_jobs":
+            workflow, jobs = ret
+            jobs_map[workflow["id"]] = jobs
 
     proc(pipelines_map, workflows_map, jobs_map)
 
